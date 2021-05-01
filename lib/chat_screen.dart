a0c4d3c6ce55ef1b0db44a66e1e2219ae623aed1
Paulo Henrique Palmeira Franco <paulohpfranco@uol.com.br>
2021-05-01 09:35:56 -0300

    Add login com o Google

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -11,11 +13,65 @@ class ChatScreen extends StatefulWidget {
   _ChatScreenState createState() => _ChatScreenState();
 }
 
-final FirebaseFirestore firestore = FirebaseFirestore.instance;
-
 class _ChatScreenState extends State<ChatScreen> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  User _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    firebaseAuth.userChanges().listen((User user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<User> _getUser() async {
+    if (_currentUser != null) {
+      return _currentUser;
+    }
+
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      final UserCredential userCredential =
+          await firebaseAuth.signInWithCredential(credential);
+
+      return userCredential.user;
+    } catch (err) {
+      print(err);
+    }
+  }
+
   void _sendMessage({String text, PickedFile imgFile}) async {
-    final Map<String, dynamic> data = <String, dynamic>{};
+    final User user = await _getUser();
+
+    if (user == null) {
+      setState(() {
+        _scaffoldKey.currentState.showBottomSheet<void>(
+            (BuildContext context) => const SnackBar(
+                content:
+                    Text('Não foi possivel fazer o login, tente novamente')));
+      });
+    }
+
+    final Map<String, dynamic> data = <String, dynamic>{
+      'uid': user.uid,
+      'senderName': user.displayName,
+      'senderPhotoURL': user.photoURL,
+    };
 
     if (imgFile != null) {
       final File file = File(imgFile.path);
@@ -45,6 +101,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: const Text('Olá'),
         elevation: 0,
